4cecbf34ad4bebee9b7b4f56050faa96682d8bd7
Bysokar <66674931+draksterau@users.noreply.github.com>
2021-07-19 18:50:57 +1000

    Baseline AR modifier

--- src/main/java/toughasnails/temperature/modifier/ARModifier.java
+++ src/main/java/toughasnails/temperature/modifier/ARModifier.java
@@ -10,39 +10,38 @@ import zmaster587.advancedRocketry.dimension.DimensionManager;
 import zmaster587.advancedRocketry.dimension.DimensionProperties;
 import zmaster587.advancedRocketry.dimension.DimensionProperties.Temps;
 
-public class ARModifier extends TemperatureModifier
-{
-    public ARModifier(String id)
-    {
-        super(id);
-    }
+public class ARModifier extends TemperatureModifier {
+	public ARModifier(String id) {
+		super(id);
+	}
 
-    @Override
-    public Temperature applyEnvironmentModifiers(World world, BlockPos pos, Temperature initialTemperature, IModifierMonitor monitor)
-    {
-        int temperatureLevel = initialTemperature.getRawValue();
-        int newTemperatureLevel = temperatureLevel;
+	@Override
+	public Temperature applyEnvironmentModifiers(World world, BlockPos pos, Temperature initialTemperature,
+			IModifierMonitor monitor) {
+		int temperatureLevel = initialTemperature.getRawValue();
+		int newTemperatureLevel = temperatureLevel;
 
-        if (world.provider.isSurfaceWorld())
-        {
-        	DimensionProperties props = DimensionManager.getEffectiveDimId(world.provider.getDimension(), pos);
-        	if (props != null) {
-        		if (props.getAverageTemp() > Temps.NORMAL.getTemp()) {
-        			newTemperatureLevel += props.getAverageTemp();
-        		} else {
-        			newTemperatureLevel -= props.getAverageTemp();
-        		}
-        	}
-        }
+		if (world.provider.isSurfaceWorld()) {
+			DimensionProperties props = DimensionManager.getEffectiveDimId(world.provider.getDimension(), pos);
+			if (props != null) {
+				int ideal = Temps.NORMAL.getTemp();
+				if (props.getAverageTemp() > ideal) {
+					newTemperatureLevel += (props.getAverageTemp() - ideal);
+				}
+				if (props.getAverageTemp() < ideal) {
+					newTemperatureLevel -= (ideal - props.getAverageTemp());
+				}
+			}
+		}
 
-        monitor.addEntry(new IModifierMonitor.Context(this.getId(), "Average Temperature", initialTemperature, new Temperature(newTemperatureLevel)));
-        
-        return new Temperature(newTemperatureLevel);
-    }
+		monitor.addEntry(new IModifierMonitor.Context(this.getId(), "Average Temperature", initialTemperature,
+				new Temperature(newTemperatureLevel)));
 
-    @Override
-    public boolean isPlayerSpecific()
-    {
-        return false;
-    }
+		return new Temperature(newTemperatureLevel);
+	}
+
+	@Override
+	public boolean isPlayerSpecific() {
+		return false;
+	}
 }
